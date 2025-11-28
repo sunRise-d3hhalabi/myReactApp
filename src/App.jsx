bb4d5746d81b765f0f7069407336e7b1b810b0f2
Dana Halabi <d3hhalabi@gmail.com>
2025-11-28 08:49:05 +0300

    7 -Filtering Data with State

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ProductGrid from "./components/ProductGrid";
@@ -11,14 +12,38 @@ const products = [
 ];
 
 function App() {
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  const showFilteredProducts = () => {
+    setFilteredProducts(products.filter((product) => product.price < 500));
+  };
+
+  const showAllProducts = () => {
+    setFilteredProducts(products);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="grow flex items-center justify-center bg-gray-100">
-        {/* <h1 className="text-3xl font-bold text-center text-blue-600">
-          Welcome to TrendyMart!
-        </h1> */}
-        <ProductGrid products={products} />
+      <main className="grow flex bg-gray-100">
+        <aside className="w-1/4 bg-white p-4 pb-4">
+          <h2 className="text-lg font-semibold mb-4">Filters</h2>
+          <button
+            className="block w-full px-4 py-2 my-2 text-white rounded-sm bg-blue-500"
+            onClick={showFilteredProducts}
+          >
+            Under $500
+          </button>
+          <button
+            className="block w-full px-4 py-2 my-2 text-white rounded-sm bg-gray-500"
+            onClick={showAllProducts}
+          >
+            Show all products
+          </button>
+        </aside>
+        <section className="grow p-4">
+          <ProductGrid products={filteredProducts} />
+        </section>
       </main>
       <Footer />
     </div>
