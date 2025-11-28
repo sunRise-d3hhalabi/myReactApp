816bebddc1b8183c2d15e346b97c3001c22ca035
Dana Halabi <d3hhalabi@gmail.com>
2025-11-28 09:06:27 +0300

    8 -Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ProductGrid from "./components/ProductGrid";
 import Gallery from "./Gallery";
+import Filter from "./components/Filter";
 
 const products = [
   { id: 1, title: "Smartphone", price: 699, image: "path/to/smartphone.jpg" },
@@ -14,33 +15,19 @@ const products = [
 function App() {
   const [filteredProducts, setFilteredProducts] = useState(products);
 
-  const showFilteredProducts = () => {
-    setFilteredProducts(products.filter((product) => product.price < 500));
-  };
-
-  const showAllProducts = () => {
-    setFilteredProducts(products);
+  const handleFilter = (filterType) => {
+    if (filterType === "under500") {
+      setFilteredProducts(products.filter((product) => product.price < 500));
+    } else if (filterType === "allProducts") {
+      setFilteredProducts(products);
+    }
   };
 
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="grow flex bg-gray-100">
-        <aside className="w-1/4 bg-white p-4 pb-4">
-          <h2 className="text-lg font-semibold mb-4">Filters</h2>
-          <button
-            className="block w-full px-4 py-2 my-2 text-white rounded-sm bg-blue-500"
-            onClick={showFilteredProducts}
-          >
-            Under $500
-          </button>
-          <button
-            className="block w-full px-4 py-2 my-2 text-white rounded-sm bg-gray-500"
-            onClick={showAllProducts}
-          >
-            Show all products
-          </button>
-        </aside>
+        <Filter onFilter={handleFilter} />
         <section className="grow p-4">
           <ProductGrid products={filteredProducts} />
         </section>
---
+++ src/components/Filter.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+export default function Filter({ onFilter }) {
+  return (
+    <aside className="w-1/4 bg-white p-4 pb-4">
+      <h2 className="text-lg font-semibold mb-4">Filters</h2>
+      <button
+        className="block w-full px-4 py-2 my-2 text-white rounded-sm bg-blue-500"
+        onClick={() => onFilter("under500")}
+      >
+        Under $500
+      </button>
+      <button
+        className="block w-full px-4 py-2 my-2 text-white rounded-sm bg-gray-500"
+        onClick={() => onFilter("allProducts")}
+      >
+        Show all products
+      </button>
+    </aside>
+  );
+}
+
+Filter.propTypes = {
+  onFilter: PropTypes.func.isRequired,
+};
