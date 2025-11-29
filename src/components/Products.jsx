b2f187d112dd127f2f5fb82688138c5197ae321a
Dana Halabi <d3hhalabi@gmail.com>
2025-11-29 03:43:19 +0300

    4 -use() API

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,36 +1,27 @@
-import { useEffect, useState } from "react";
+import { Suspense, use } from "react";
+// import { useEffect, useState } from "react";
 import ProductGrid from "./ProductGrid";
 import axios from "axios";
 
-export default function Products(params) {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // console.log("call from useEffect");
-    async function fetchProducts() {
-      try {
-        const response = await axios.get("https://fakestoreapi.com/products");
-        //   console.log(response);
-        setProducts(response.data);
-      } catch (error) {
-        alert("Error fetching products!");
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchProducts();
-  }, []);
-
-  if (loading) {
-    <div>Loading products ...</div>;
+const fetchProducts = async () => {
+  try {
+    const { data } = await axios.get("https://fakestoreapi.com/products");
+    return data;
+  } catch (error) {
+    alert("Error fetching products!");
+    console.log(error);
   }
+};
+const productsPromise = fetchProducts();
+
+export default function Products(params) {
+  const products = use(productsPromise);
 
   return (
     <section className="grow p-4">
-      <ProductGrid products={products} />
+      <Suspense fallback={<div>Loading products ...</div>}>
+        <ProductGrid products={products} />
+      </Suspense>
     </section>
   );
 }
